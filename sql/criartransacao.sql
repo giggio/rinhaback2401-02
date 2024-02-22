ceb4163b577b78139b8ff89c2cfb244db5a236f9
Giovanni Bassi <giggio@giggio.net>
2024-02-22 12:45:54 -0300

    Optimize db

--- sql/criartransacao.sql
+++ sql/criartransacao.sql
@@ -24,20 +24,17 @@ BEGIN
     RETURN ret;
   END IF;
 
-  --raise notice'Criando transacao para cliente %.', idcliente;
-  INSERT INTO transacao (valor, descricao, realizadaem, idcliente)
-    VALUES (valor, descricao, now() at time zone 'utc', idcliente);
   UPDATE cliente
     SET saldo = saldo + valor
     WHERE id = idcliente AND (valor > 0 OR saldo + valor >= limite)
     RETURNING saldo, limite
     INTO search;
-  raise notice'search: %', search;
   IF search.limite is NULL THEN
-    --raise notice'Id do Cliente % não encontrado.', idcliente;
     SELECT -2, 0, 0 INTO ret;
     RETURN ret;
   ELSE
+    INSERT INTO transacao (valor, descricao, idcliente)
+      VALUES (valor, descricao, idcliente);
     SELECT 0, search.saldo, search.limite INTO ret;
   END IF;
   RETURN RET;
